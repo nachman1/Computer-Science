6b583226c3df54d6f1a71375e7d79add1cfbec22
Nachman_Zohar <73222862+nachman1@users.noreply.github.com>
2023-10-16 12:44:12 +0300

    משימה סוכות

---
+++ Main (copy) (1).java
@@ -0,0 +1,66 @@
+//class Main {
+  //public static void main(String[] args) {
+      //int x = 4,y = 5,z = 67;
+    //System.out.println(x+"\n"+y+"\n"+z);
+  //}
+//}
+
+//class Main {
+  //public static void main(String[] args) {
+    //float x = 56;
+      //int y = 5;
+    //System.out.println(x+"   "+y);
+  //}
+//}
+
+//class Main {
+  //public static void main(String[] args) {
+    //char x = 'N';
+    //char y = 'a';
+    //char z = 'c';
+    //char a = 'h';
+    //char b = 'm';
+    //char c = 'a';
+    //char d = 'n';
+      //System.out.println(x+""+y+""+z+""+a+""+b+""+c+""+d);
+  //}
+//}
+
+//class Main {
+  //public static void main(String[] args) {
+    //int x = 6, y = 7;
+    //System.out.println(x+" is less then "+y);
+  //}
+//}
+
+//class Main {
+  //public static void main(String[] args) {
+      //int x = 25, y = 6, c = 14;
+      //float numAve = (float)(x+y+c)/2;
+      //System.out.println("numbers Avearage is "+ numAve);
+  //}  
+//}
+
+//class Main {
+  //public static void main(String[] args) {
+    //int a = 5, b = 8;
+    //int c = (a*a) + (b*b);
+    //System.out.println("c power 2 = "+ c);
+  //}
+//}
+
+//class Main {
+  //public static void main(String[] args) {
+    //int x = 45; 
+    //int y =(2*x+5);
+    //System.out.println(y);
+  //}
+//}
+
+//class Main {
+  //public static void main(String[] args) {
+    //String str1 =  "I like to study computer science", str2 = " and therefore ready to invest ",      //str3 = " hours a day for practice";
+    //int hours = 5;
+    //System.out.println(str1+ str2 + hours+ str3);
+  //}
+//}
