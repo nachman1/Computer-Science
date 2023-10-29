11697fa39580b98782c75dc6e30a9bb43f796204
Nachman_Zohar <73222862+nachman1@users.noreply.github.com>
2023-10-29 13:42:12 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+/*import java.util.Scanner;
+class Main {
+  public static void main(String[] args) { 
+    System.out.println("Enter Your Name");
+    Scanner scan = new Scanner(System.in);
+    String Name = scan.nextLine();
+    System.out.println(Name);
+    System.out.println("Enter Your Age");
+    int Age = scan.nextInt();
+    System.out.println(Age);
+  }
+}*/
+
+/*import java.util.Scanner;
+class Main {
+  public static void main(String[] args) { 
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter Number");
+    int Num = scan.nextInt();
+    if (Num > 0)
+    {
+    System.out.println("the number is positive");
+    }
+    else
+    {
+    System.out.println("the number is negative");
+    }
+  }
+}*/
